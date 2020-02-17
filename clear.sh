01c32ae4d5fc1f31aee7fd4f2103fd2c725d7b72
Felix Galaup <felixgalaup@yahoo.fr>
2020-02-17 11:48:19 +0100

    Improve file setup a clear. + Update header.

--- clear.sh
+++ clear.sh
@@ -1,15 +1,15 @@
 # **************************************************************************** #
-#                                                           LE - /             #
-#                                                               /              #
-#    clear.sh                                         .::    .:/ .      .::    #
-#                                                  +:+:+   +:    +:  +:+:+     #
-#    By: fgalaup <fgalaup@student.le-101.fr>        +:+   +:    +:    +:+      #
-#                                                  #+#   #+    #+    #+#       #
-#    Created: 2020/02/05 15:19:43 by fgalaup      #+#   ##    ##    #+#        #
-#    Updated: 2020/02/05 15:37:07 by fgalaup     ###    #+. /#+    ###.fr      #
-#                                                          /                   #
-#                                                         /                    #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    clear.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: fgalaup <fgalaup@student.le-101.fr>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/02/05 15:19:43 by fgalaup           #+#    #+#              #
+#    Updated: 2020/02/17 11:46:57 by fgalaup          ###   ########lyon.fr    #
+#                                                                              #
 # **************************************************************************** #
+
 
 #!/bin/sh
 
@@ -22,11 +22,12 @@ kubectl delete -k srcs
 sh ./stop.sh
 
 # Delete the cluster
-minikube delete
+minikube delete --all
 
 # Optional if you want clear all
 if [ "$1" = "--all" ]
 then
+    minikube delete --purge=true
     brew remove minikube
     brew remove docker
     brew clean
--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 # **************************************************************************** #
-#                                                           LE - /             #
-#                                                               /              #
-#    setup.sh                                         .::    .:/ .      .::    #
-#                                                  +:+:+   +:    +:  +:+:+     #
-#    By: fgalaup <fgalaup@student.le-101.fr>        +:+   +:    +:    +:+      #
-#                                                  #+#   #+    #+    #+#       #
-#    Created: 2020/01/19 16:30:48 by fgalaup      #+#   ##    ##    #+#        #
-#    Updated: 2020/02/06 10:04:41 by fgalaup     ###    #+. /#+    ###.fr      #
-#                                                          /                   #
-#                                                         /                    #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    setup.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: fgalaup <fgalaup@student.le-101.fr>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/01/19 16:30:48 by fgalaup           #+#    #+#              #
+#    Updated: 2020/02/17 11:46:45 by fgalaup          ###   ########lyon.fr    #
+#                                                                              #
 # **************************************************************************** #
+
 
 #!/bin/sh
 
@@ -17,21 +17,21 @@
 
 # Check if dependency is installed and install.
 # Brew
-if ! which brew >/dev/null 2>&1
+if ! which brew > /dev/null 2>&1
 then
     # Install Brew
     curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh
 fi
 
-# Docker (Devloppment Only)
-if ! which docker >/dev/null 2>&1
+# Docker
+if ! which docker > /dev/null 2>&1
 then
     # Install Docker
     brew install docker
 fi
 
 # Minikube
-if ! which minikube >/dev/null 2>&1
+if ! which minikube > /dev/null 2>&1
 then
     # Install Minikube
     brew install minikube
@@ -40,20 +40,27 @@ fi
 # =============================== RUN MINIKUBE =============================== #
 
 # Check if minikube is 
-if ! minikube status >/dev/null 2>&1
+if ! minikube status > /dev/null 2>&1
 then
     echo "Start Minikube ..."
     if ! minikube start --vm-driver=virtualbox \
-        --cpus 3 --disk-size=30000mb --memory=3000mb # \
+        --cpus 3 --disk-size=30000mb --memory=3000mb
+        # \
         # --bootstrapper=kubeadm # allow telegraf to query metrics
+        # -p ft_services # add name
     then
         echo "Cannot start minikube!"
         exit 1
     fi
 
     # Make search to enable
+    minikube addons enable ingress
     # minikube addons enable metrics-server
-    # minikube addons enable ingress
 fi
 
 # ================================== DEPLOY ================================== #
+
+# Linking client docker with docker in virtual box
+eval $(minikube docker-env)
+
+# Build docker image
