44e1f2ca5749f0064577775194c56dbc0b3a7ec1
Felix Galaup <felixgalaup@yahoo.fr>
2020-02-24 18:02:06 +0100

    Improve Test And remove usless comment

--- srcs/MySQL/Test.sh
+++ srcs/MySQL/Test.sh
@@ -6,11 +6,13 @@
 #    By: fgalaup <fgalaup@student.le-101.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/17 11:45:34 by fgalaup           #+#    #+#              #
-#    Updated: 2020/02/19 16:49:42 by fgalaup          ###   ########lyon.fr    #
+#    Updated: 2020/02/24 13:10:04 by fgalaup          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
+
+eval $(minikube docker-env);
 
 # Creating docker image
 docker image build --tag services_mysql:1.0 .
@@ -27,3 +29,4 @@ sleep 5;
 docker logs mysql
 
 # To connect to the MySQL
+# ~/.brew/Cellar/mysql-client/8.0.18/bin/mysql -h 192.168.99.102 -u root --password="root"
