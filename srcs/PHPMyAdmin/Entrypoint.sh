3654277312a8c3913c47c161660b9a61490a8763
Felix Galaup <felixgalaup@yahoo.fr>
2020-02-24 18:26:26 +0100

    Creating the database of phpmyadmin

--- srcs/PHPMyAdmin/Entrypoint.sh
+++ srcs/PHPMyAdmin/Entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: fgalaup <fgalaup@student.le-101.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/23 14:43:30 by fgalaup           #+#    #+#              #
-#    Updated: 2020/02/24 17:54:05 by fgalaup          ###   ########lyon.fr    #
+#    Updated: 2020/02/24 18:21:26 by fgalaup          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,11 +20,12 @@ export PHP_ERROR_REPORTING="E_COMPILE_ERROR\|E_RECOVERABLE_ERROR\|E_ERROR\|E_COR
 /usr/sbin/php-fpm7 -d clear_env=no
 
 # Install PHPMyAdmin Database
-if []
-	if [[ -z "`mysql -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$PMA_PMADB'" 2>&1`" ]];
+if [ -z "$PMA_PMADB" ] && [ -z "$PMA_HOST" ] && [ -z "$PMA_USER" ] ;
+then
+	if [[ -z "`mysql -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$PMA_PMADB'"`" ]];
 	then
-		mysql --host=${PMA_HOST} --user=${PMA_USER} --password=${PMA_PASSWORD} -e "CREATE DATABASE $PMA_PMADB;" && \
-		mysql --host=${PMA_HOST} --user=${PMA_USER} --password=${PMA_PASSWORD} ${PMA_PMADB} < phpmyadmin.sql
+		mysql --host=${PMA_HOST} --user=${PMA_USER} --password=${PMA_PASSWORD} -e "CREATE DATABASE $PMA_PMADB;"
+		mysql --host=${PMA_HOST} --user=${PMA_USER} --password=${PMA_PASSWORD} ${PMA_PMADB} < /Application/PHPMyAdmin/sql/create_tables.sql
 	fi
 fi
 
