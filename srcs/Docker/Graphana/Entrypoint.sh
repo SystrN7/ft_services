16a6bfc2b63aa81ba92a1ba042cef6f3a951edb3
Felix Galaup <felixgalaup@yahoo.fr>
2020-06-08 15:49:12 +0200

    [^] - [Docker](Grafana) - trying to make working version Fail

--- srcs/Docker/Graphana/Entrypoint.sh
+++ srcs/Docker/Graphana/Entrypoint.sh
@@ -3,11 +3,12 @@
 #                                                         :::      ::::::::    #
 #    Entrypoint.sh                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: fgalaup <fgalaup@student.le-101.fr>        +#+  +:+       +#+         #
+#    By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/16 11:26:30 by fgalaup           #+#    #+#              #
-#    Updated: 2020/03/25 11:34:04 by fgalaup          ###   ########lyon.fr    #
+#    Updated: 2020/06/06 18:29:26 by fgalaup          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
-make run
+chown -R grafana:grafana /Application/Grafana
+grafana-server --homepath=/Application/Grafana
