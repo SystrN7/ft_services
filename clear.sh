b1b1ae9eece5242b1861037e6d5c6130f127b87c
Felix Galaup <felixgalaup@yahoo.fr>
2020-06-17 14:46:28 +0200

    [^] - [Clear](Dependency) - Change for linux.

--- clear.sh
+++ clear.sh
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    clear.sh                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: fgalaup <fgalaup@student.le-101.fr>        +#+  +:+       +#+         #
+#    By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/05 15:19:43 by fgalaup           #+#    #+#              #
-#    Updated: 2020/03/08 10:09:55 by fgalaup          ###   ########lyon.fr    #
+#    Updated: 2020/06/16 15:31:10 by fgalaup          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -26,18 +26,17 @@ kubectl delete ingress ingress-redirect
 
 if [ "$1" = "--minikube" ] || [ "$1" = "--all" ]
 then
-# Stop minikube
-sh ./stop.sh
+	# Stop minikube
+	sh ./stop.sh
 
-# Delete the cluster
-minikube delete --all
+	# Delete the cluster
+	minikube delete --all
 fi
 
 # Optional if you want clear all
 if [ "$1" = "--all" ]
 then
-    minikube delete --purge=true
-    brew remove minikube
-    brew remove docker
-    brew clean
+	minikube delete --purge=true
+	sudo apt remove minikube
+	sudo apt remove docker
 fi
