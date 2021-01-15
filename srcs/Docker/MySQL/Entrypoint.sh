be9905389b133420e35ed0360f4c65ac2e68205d
Felix Galaup <felixgalaup@yahoo.fr>
2021-01-15 10:59:58 +0100

    [^]-[Docker](MySQL)-Fix loop if database allredy exits.

--- srcs/Docker/MySQL/Entrypoint.sh
+++ srcs/Docker/MySQL/Entrypoint.sh
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Entrypoint.sh                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
+#    By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/17 15:23:52 by fgalaup           #+#    #+#              #
-#    Updated: 2021/01/13 15:44:40 by felix            ###   ########lyon.fr    #
+#    Updated: 2021/01/15 09:55:33 by fgalaup          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -27,21 +27,24 @@ then
 	mkdir -p "$MYSQL_DATA_PATH"
 fi
 
+# Init Database
 if [ ! -d $MYSQL_DATA_PATH/mysql ]
 then
+	echo "Init Database"
 	mysql_install_db --datadir="${MYSQL_DATA_PATH}" --user="${MYSQL_ROOT_NAME}"
+
+	echo "Creating root users"
+	# Creating root user for remote access to the databases.
+	echo "FLUSH PRIVILEGES;" > /tmps
+	echo "CREATE USER '$MYSQL_ROOT_NAME'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> /tmps
+	echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ROOT_NAME'@'%' WITH GRANT OPTION;" >> /tmps
+
+	if ! /usr/bin/mysqld --datadir="${MYSQL_DATA_PATH}" --user="${MYSQL_ROOT_NAME}" --bootstrap --verbose=0 < /tmps
+	then
+		exit 1
+	fi
 else
-	echo "Database allredy exist."
-fi
-
-# Creating root user for remote access to the databases.
-echo "FLUSH PRIVILEGES;" > /tmps
-echo "CREATE USER '$MYSQL_ROOT_NAME'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> /tmps
-echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ROOT_NAME'@'%' WITH GRANT OPTION;" >> /tmps
-
-if ! /usr/bin/mysqld --datadir="${MYSQL_DATA_PATH}" --user="${MYSQL_ROOT_NAME}" --bootstrap --verbose=0 < /tmps
-then
-	exit 1
+	echo "Database allready exist."
 fi
 
 # Start Mysql
